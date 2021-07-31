ae388902da764e34a4930ba15567a0464fb41692
Eva Walls <evawalls@evas-mbp.lan>
2021-07-30 21:43:52 -0400

    Going got GOLD

--- src/Helpers/environment.tsx
+++ src/Helpers/environment.tsx
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
     // this is the deployed react application
-    case 'eva-plantr.herokuapp.com';
+    case 'eva-plantr.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://eva-plantr.herokuapp.com'
 }
